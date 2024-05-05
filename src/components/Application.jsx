6f8fdf91095f75fef2907eb8273e0e76a2c1c88f
Om Prakash Mallik <opm202000@gmail.com>
2024-05-05 14:52:37 +0530

    letss goo

---
+++ src/components/Application.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Application() {
+    return (
+        <div className='bg-neutral-300'>
+            <section>Left</section>
+            <section>Home</section>
+            <section>Right</section>
+        </div>
+    )
+}
+
+export default Application
